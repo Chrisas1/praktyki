3e55cdffb4a8a254cbcf31673d3d10ecce76bbc5
Krzysztof <gctgctwwc@gmail.com>
2017-05-11 15:42:29 +0200

    working models

--- praktyki/cal/models.py
+++ praktyki/cal/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+import datetime
 
 # Create your models here.
 class Client(models.Model):
@@ -11,11 +12,18 @@ class Project(models.Model):
     code = models.CharField(max_length=8, unique=True)
     client = models.ForeignKey(Client, on_delete=models.CASCADE)
 
+
 class Task(models.Model):
     name = models.CharField(max_length=40)
-    description = models.CharField(max_length=100)
+    description = models.CharField(max_length=100, blank=True)
     to_do_time = models.IntegerField()
-    start = models.DateField('Start time')
-    end = models.DateField('End time')
+    start = models.DateTimeField('Start time')
+    end = models.DateTimeField('End time')
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def is_continuous(self):
+        if self.end - self.start <= datetime.timedelta(hours=self.to_do_time):
+            return True
+        else:
+            return False
